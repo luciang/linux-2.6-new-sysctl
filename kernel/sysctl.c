70ab95aeab4913ce1fd6cbe04eb70952970b4a44
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2011-05-01 21:24:18 +0200

    sysctl: call sysctl_init before the first sysctl registration
    In the next patch key_init() will be changed to register a sysctl
    table. In preparation, we call sysctl_init() before it.
    
    Also, rename net/sysctl_net.c's sysctl_init so the two don't clash.
    
    Signed-off-by: Lucian Adrian Grijincu <lucian.grijincu@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1716,7 +1716,7 @@ static void sysctl_set_parent(struct ctl_table *parent, struct ctl_table *table)
 	}
 }
 
-static __init int sysctl_init(void)
+__init int sysctl_init(void)
 {
 	sysctl_set_parent(NULL, root_table);
 #ifdef CONFIG_SYSCTL_SYSCALL_CHECK
@@ -1724,8 +1724,6 @@ static __init int sysctl_init(void)
 #endif
 	return 0;
 }
-
-core_initcall(sysctl_init);
 
 static struct ctl_table *is_branch_in(struct ctl_table *branch,
 				      struct ctl_table *table)
