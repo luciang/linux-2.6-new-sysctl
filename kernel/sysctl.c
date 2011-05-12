91942b95cc8c709c3f04525248a3e6b577df3f63
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2011-05-12 18:39:09 +0200

    sysctl: introduce temporary sysctl wrappers
    These wrappers are only needed until the .child member of ctl_table
    get's removed from all the kernel.
    
    Signed-off-by: Lucian Adrian Grijincu <lucian.grijincu@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1838,7 +1838,7 @@ static void try_attach(struct ctl_table_header *p, struct ctl_table_header *q)
  * This routine returns %NULL on a failure to register, and a pointer
  * to the table header on success.
  */
-struct ctl_table_header *__register_sysctl_paths(
+static struct ctl_table_header *__register_sysctl_paths_impl(
 	struct ctl_table_root *root,
 	struct nsproxy *namespaces,
 	const struct ctl_path *path, struct ctl_table *table)
@@ -1914,6 +1914,14 @@ struct ctl_table_header *__register_sysctl_paths(
 	return header;
 }
 
+struct ctl_table_header *__register_sysctl_paths(
+	struct ctl_table_root *root,
+	struct nsproxy *namespaces,
+	const struct ctl_path *path, struct ctl_table *table)
+{
+	return __register_sysctl_paths_impl(root, namespaces, path, table);
+}
+
 /**
  * register_sysctl_table_path - register a sysctl table hierarchy
  * @path: The path to the directory the sysctl table is in.
@@ -1954,7 +1962,7 @@ struct ctl_table_header *register_sysctl_table(struct ctl_table *table)
  * Unregisters the sysctl table and all children. proc entries may not
  * actually be removed until they are no longer used by anyone.
  */
-void unregister_sysctl_table(struct ctl_table_header * header)
+static void unregister_sysctl_table_impl(struct ctl_table_header * header)
 {
 	might_sleep();
 
@@ -1970,6 +1978,11 @@ void unregister_sysctl_table(struct ctl_table_header * header)
 	if (!--header->count)
 		call_rcu(&header->rcu, free_head);
 	spin_unlock(&sysctl_lock);
+}
+
+void unregister_sysctl_table(struct ctl_table_header * header)
+{
+	return unregister_sysctl_table_impl(header);
 }
 
 int sysctl_is_seen(struct ctl_table_header *p)
